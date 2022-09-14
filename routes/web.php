1d3df783e47dc8eac03b37051fceb9c0b13bbcd5
SaBobic <fabrizioettori@gmail.com>
2022-09-14 16:53:24 +0200

    added views and routing

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
-});
+    $title = 'Hello World';
+    return view('home', compact('title'));
+})->name('homepage');
+
+Route::get('/services', function () {
+    $title = 'Services';
+    return view('services', compact('title'));
+})->name('services');
+
+Route::get('/case-studies', function () {
+    $title = 'Case Studies';
+    return view('casestudies', compact('title'));
+})->name('case-studies');
+
+Route::get('/meet-the-team', function () {
+    $title = 'Meet The Team';
+    return view('meettheteam', compact('title'));
+})->name('meet-the-team');
+
+Route::get('/news', function () {
+    $title = 'News';
+    return view('news', compact('title'));
+})->name('news');
+
+Route::get('/get-in-touch', function () {
+    $title = 'Get In Touch';
+    return view('getintouch', compact('title'));
+})->name('get-in-touch');
